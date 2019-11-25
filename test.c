fb3210f5e679a0c1084002574d0afe7c9b54a52f
wanghuahuai <1600174041@qq.com>
2019-11-25 15:19:25 +0800

    test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: wanghuahua 
+	#	 Email		: 1600174041.qq.com 
+	#	 Created	: Mon 25 Nov 2019 03:14:56 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	int a;
+    return 0;
+}
