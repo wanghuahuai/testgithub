0dfd2c917f1738876c4f7d4cca0b45d999b2ad1e
wanghuahuai <1600174041@qq.com>
2019-11-25 15:51:27 +0800

    dome.c

---
+++ dome.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	#	 FileName	: dome.c
+	#	 Author		: wanghuahua 
+	#	 Email		: 1600174041.qq.com 
+	#	 Created	: Mon 25 Nov 2019 03:50:43 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{   int b ;
+    return 0;
+}
